ec98d7bb8a703f52a5845a7a9f79cae7d2a39f19
Your Name <you@example.com>
2023-02-19 12:17:59 +0600

    project setup

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+const Home = () => {
+    return (
+        <div>
+            <Typography variant='h1'>
+                home
+            </Typography>
+        </div>
+    );
+};
+
+export default Home;
