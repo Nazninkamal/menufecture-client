923a8723e72481e307756d7c23aa97cde9c69d78
Your Name <you@example.com>
2023-02-22 20:00:25 +0600

    tailwind css setup

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,29 +1,18 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
-// import Navbar from '../../../Components/Navber/Navber';
 import { useGetProductsQuery } from '../../../Redux/Features/products/productApi';
-import Banner from '../Banner/Banner';
-import HowToOrder from '../HowToOrder/HowToOrder';
-import Services from '../Services/Services';
-import Application from '../Applications/Application';
-import Footer from '../../Shared/Footer/Footer';
-import Navigation from '../../Shared/Navigation/Navigation';
-// import { Navigation } from '@mui/icons-material';
+
 const Home = () => {
     const { data: products, isLoading, isSuccess, isError } = useGetProductsQuery();
 
     console.log(products?.result);
 
     return (
-        <Box>
-           <Navigation /> 
-            <Banner/>
-            <Services/>
-            <Application/>
-            <HowToOrder/>
-            <Footer/>
-            
-        </Box>
+        <div>
+            <h1 className="text-3xl font-bold underline text-green-400 flex justify-center items-center h-screen">
+                Hello world!
+            </h1>
+
+        </div>
     );
 };
 
