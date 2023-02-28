778c1978329ffc4bc76eab33f8796223e4e1ba21
Naznin Kamal <najninkamalrimi@gmail.com>
2023-03-01 00:21:10 +0600

    dashboardLayout-added

--- src/Routers/Routers.js
+++ src/Routers/Routers.js
@@ -1,13 +1,27 @@
 import { createBrowserRouter, } from "react-router-dom";
-import Home from "../Pages/Home/Home/Home";
+import DashboardLayout from "../Pages/Dashboard/DashboardLayout";
+import Projects from "../Pages/Dashboard/Projects/Projects";
+import User from "../Pages/User/User";
 
 
 export const routers = createBrowserRouter([
     {
         path: "/",
-        element:
-            <Home />
+        element: <DashboardLayout/>,
+        children : [
+           {
+             path: '/user',
+            element: <User/>
+           },
+           {
+            path: "project",
+            element: <Projects/>,
+           
+        },
+        ]
     },
+    
+    
 
 
 
