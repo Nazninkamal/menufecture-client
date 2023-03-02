2cf8179d2f3e6e8a05df078ee3f2a9676afde327
Naznin Kamal <najninkamalrimi@gmail.com>
2023-03-02 21:55:58 +0600

    SharedBar Added

--- src/Routers/Routers.js
+++ src/Routers/Routers.js
@@ -1,7 +1,8 @@
 import { createBrowserRouter, } from "react-router-dom";
 import DashboardHome from "../Dashboard/DashboardHome";
 import DashboardLayout from "../Dashboard/DashboardLayout";
-import NewProject from "../Pages/NewProject/NewProject";
+import NewProject from "../Pages/Projects/NewProject";
+// import NewProject from "../Pages/NewProject/NewProject";
 import Projects from "../Pages/Projects/Projects";
 import User from "../Pages/User/User";
 
