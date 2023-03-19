ddb26d524f6324538e38969be8ce235f14db7785
Naznin Kamal <najninkamalrimi@gmail.com>
2023-03-19 22:15:55 +0600

    feedback and contact route added

---
+++ src/Pages/User/Contact/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import SharedBar from '../../../Components/SharedBar/SharedBar';
+
+const Contact = () => {
+    return (
+        <div>
+
+        <h2 class="my-4 text-5xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
+      <p class="mb-8 lg:mb-16 font-light text-center text-gray-900 dark:text-gray-900 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
+        </div>
+    );
+};
+
+export default Contact;
