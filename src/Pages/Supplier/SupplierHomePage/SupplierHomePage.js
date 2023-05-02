55f84b53aa910ce6a172c78b127630929e713d47
Your Name <you@example.com>
2023-05-02 10:48:47 +0600

    rating cart

--- src/Pages/Supplier/SupplierHomePage/SupplierHomePage.js
+++ src/Pages/Supplier/SupplierHomePage/SupplierHomePage.js
@@ -1,4 +1,4 @@
-import React, {useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useState } from "react";
 import SharedBar from '../../../Components/SharedBar/SharedBar';
 import PiaChart from "../../Carts/PieChart";
@@ -9,60 +9,64 @@ import { useGetFeedbackQuery } from '../../../Redux/Features/Feedback/feedbackAp
 
 
 const SupplierHomePage = () => {
+  const [userRating, setUserRating] = useState(undefined);
+  const { data, isSuccess,  } = useGetFeedbackQuery();
 
-  const { data, isSuccess } = useGetFeedbackQuery();
 
 
+ /*  if(isLoading){
+    return
+  } */
   const rating5 = data?.result?.filter((data) => data?.rating === 5);
   const rating4 = data?.result?.filter((data) => data?.rating >= 4 && data?.rating < 5);
   const rating3 = data?.result?.filter((data) => data?.rating >= 3 && data?.rating < 4);
   const rating2 = data?.result?.filter((data) => data?.rating >= 2 && data?.rating < 3);
   const rating1 = data?.result?.filter((data) => data?.rating >= 0 && data?.rating < 2);
-  console.log(rating4)
 
 
 
+
+
+
+
+ 
 
   const userRatingData = [
     {
       id: 1,
       name: "One Star Rating",
-      rating: rating1?.length,
+      rating: rating1?.length || 0,
     },
     {
       id: 2,
       name: "Two Star Rating",
-      rating: rating2?.length,
+      rating: rating2?.length || 0,
     },
     {
       id: 3,
       name: "Three Star Rating",
-      rating: rating3?.length,
+      rating: rating3?.length || 0,
     },
     {
       id: 4,
       name: "Four Star Rating",
-      rating: rating4?.length,
+      rating: rating4?.length || 0,
     },
     {
       id: 5,
       name: "Five Star Rating",
-      rating: rating5?.length,
+      rating: rating5?.length || 0,
     },
   ];
 
-
-
-  const [userRating, setUserRating] = useState({});
-
-
   useEffect(() => {
+ 
     setUserRating({
       labels: userRatingData?.map((data) => data?.name),
       datasets: [
         {
           label: "Total Rating",
-          data: rating5?.length && userRatingData?.map((data) => data?.rating),
+          data: userRatingData?.map((data) => data?.rating),
           backgroundColor: [
             "red",
             "black",
@@ -76,10 +80,11 @@ const SupplierHomePage = () => {
       ],
 
     })
+
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [rating5?.length])
+  }, [isSuccess])
 
-
+console.log(userRating);
 
   return (
     <div className=' h-screen overflow-y-auto'>
@@ -88,12 +93,11 @@ const SupplierHomePage = () => {
         <SupplierCards />
       </div>
 
-
       <div className="grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-10 mt-10">
 
         <div style={{ width: "100%" }} className="w-full bg-white ">
           <p className="mb-5 text-center">Customer Satisfaction rating </p>
-          {isSuccess &&
+          {!userRating ? <h1>Loading</h1> :
 
             <PiaChart chartData={userRating} />
           }
